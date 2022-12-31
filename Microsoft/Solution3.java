9d3f9099f8a317766a9589a3d6a235737f2d93f5
rockharshitmaurya <rockharshitmaurya@gmail.com>
2023-01-01 01:05:54 +0530

    #Solution3

---
+++ Microsoft/Solution3.java
@@ -0,0 +1,26 @@
+class Solution {
+    // https://leetcode.com/problems/bulls-and-cows/
+    public String getHint(String secret, String guess) {
+        int map[] = new int[10];
+
+        for (char ch : secret.toCharArray()) {
+            map[ch - '0']++;
+        }
+        int cows = 0, bulls = 0;
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                cows++;
+                map[secret.charAt(i) - '0']--;
+            }
+        }
+
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) != guess.charAt(i)) {
+                if (map[guess.charAt(i) - '0'] > 0) bulls++;
+                map[guess.charAt(i) - '0']--;
+            }
+        }
+
+        return cows + "A" + bulls + "B";
+    }
+}
